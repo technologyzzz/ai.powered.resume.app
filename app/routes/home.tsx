3bd863d6d9517e8b12201d9bc0f92b1cc443b59d
Maulana Abdul Aziz <azizcuber03@gmail.com>
2025-09-13 22:12:50 +0700

    13sep

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVku" },
+    { name: "description", content: "Never LOSE on your dream JOB!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg']) bg-cover">
+    <Navbar />
+
+    <section className="main-section">    
+      <div className="page-heading">
+        <h1>Track your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
+  </main>
 }
