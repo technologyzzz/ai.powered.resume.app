4e5d65f7ea1c30867ae6c221aea4b50887ceff0e
Maulana Abdul Aziz <azizcuber03@gmail.com>
2025-09-15 05:16:03 +0700

    15sep

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,10 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/navbar";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/resumecard";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,14 +14,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg']) bg-cover">
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
 
     <section className="main-section">    
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
+
+      {resumes.length > 0 && ( 
+        <div className="resumes-section"> 
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
     </section>
   </main>
 }
